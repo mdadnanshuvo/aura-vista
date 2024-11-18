91dda742d0132065e42a53b783098135f4bfa006
unknown <ahmedadnanshuvo@gmail.com>
2024-11-19 00:06:01 +0600

    The layout updated

--- app/page.tsx
+++ app/page.tsx
@@ -15,60 +15,35 @@ import AmenitiesDetails from "@/components/amenitiesDetails";
 
 export default function Home() {
   return (
+   
     <div className="max-w-7xl mx-auto">
-      {/* Image Gallery */}
-      <ImageGallery />
+    {/* Image Gallery */}
+    <ImageGallery />
 
-      {/* Main Content */}
-      <div className="px-6">
-        {/* Tabs Section */}
-        <TabsSection />
+    {/* Main Content */}
+    <div className="px-6">
+      {/* Tabs Section */}
+      <TabsSection />
 
-        {/* Two Column Layout */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          {/* Left Column - Main Content */}
-          <div className="lg:col-span-2">
-            {/* Property Details Section */}
-            <PropertyDetails />
+      {/* Two Column Layout */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {/* Left Column - Main Content */}
+        <div className="lg:col-span-2">
+          <PropertyDetails />
+          <AmenitiesSection />
+          <MapSection />
+        </div>
 
-            {/* Rooms and Beds Section */}
-            <RoomsAndBeds />
-
-            {/* About Property Section */}
-            <AboutProperty />
-
-            {/* Amenities Section */}
-            <AmenitiesSection />
-
-            {/* House Rules */}
-            <HouseRules />
-
-            {/* Important Information */}
-            <ImportantInfo />
-
-            {/* Map Section */}
-            <MapSection />
-
-            {/* FAQ Section */}
-            <FAQSection />
-
-            {/* Question Section */}
-            <QuestionSection />
-          </div>
-
-          {/* Right Column - Booking Section */}
-          <div className="lg:col-span-1">
-            <BookingSection />
-            <div className="mt-4 text-right text-sm text-gray-600">
-              <div>Contact host</div>
-              <div>Property # 9838104ha</div>
-            </div>
-
-            {/* Damage and Cancellation */}
-            <DamageAndCancellation />
+        {/* Right Column - Booking Section */}
+        <div className="lg:col-span-1">
+          <BookingSection />
+          <div className="mt-4 text-right text-sm text-gray-600">
+            <div>Contact host</div>
+            <div>Property # 9838104ha</div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
